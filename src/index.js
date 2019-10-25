13bd3fb547be509786b09e63d7e585f185d74a72
Estelle Olawinski <45394713+Olawinski-e@users.noreply.github.com>
2019-10-25 22:35:54 +0200

    copy an object

--- src/index.js
+++ src/index.js
@@ -8,14 +8,105 @@ import "./styles.css";
 // let name2 = "Ana";
 // console.log(name1 === name2); // <== true
 
-const book1 = {
-  author: "Charlotte Bronte"
-};
+// const book1 = {
+//   author: "Charlotte Bronte"
+// };
+// const book2 = Object.assign({}, book1);
 
-const book2 = Object.assign({}, book1);
+// console.log(book2);
+// console.log(book1 === book2);
 
-console.log(book2); // <== { author: "Charlotte Bronte" }
-console.log(book1 === book2); // <== false
+// book1.author = "Emily Bronte";
+// console.log(book1); // <== { author: 'Emily Bronte' }
+// console.log(book2); // <== { author: 'Charlotte Bronte' }
+
+// const book1 = {
+//   author: "Charlotte Bronte",
+//   publishers: [
+//     {
+//       companyName: "AB"
+//     },
+//     {
+//       companyName: "CD"
+//     }
+//   ]
+// };
+// const book3 = Object.assign({}, book1);
+
+// book1.publishers[0] = {
+//   companyName: "Super Cool Company"
+// };
+// book1.author = "Test Test";
+// console.log(book3);
+
+// const book1 = {
+//   author: "Charlotte Bronte"
+// }
+// const book4 = {};
+
+// for(let prop in book1){
+//   book4[prop] = book1[prop]
+// }
+
+// book1.author = "William Shakespeare";
+// console.log(book1);
+// console.log(book4);
+
+// const book1 = {
+//   author: "Charlotte Bronte",
+//   publishers: [
+//     {
+//       companyName: "AB"
+//     },
+//     {
+//       companyName: "CD"
+//     }
+//   ]
+// };
+
+// function cloneObject(object) {
+//   let clone = {};
+//   for (let prop in object) {
+//     if (object[prop] != null && typeof object[prop] === "object") {
+//       clone[prop] = cloneObject(object[prop]);
+//     } else {
+//       clone[prop] = object[prop];
+//     }
+//   }
+//   return clone;
+// }
+
+// let book4 = cloneObject(book1);
+
+// book1.publishers[0] = {
+//   companyName: "Super Cool Company"
+// };
+// book1.author = "William Shakespeare";
+// console.log(book1);
+// console.log(book4);
+
+// const book1 = {
+//   author: "Charlotte Bronte",
+//   publishers: [
+//     {
+//       companyName: "AB"
+//     },
+//     {
+//       companyName: "CD"
+//     }
+//   ]
+// };
+
+// const book5 = JSON.parse(JSON.stringify(book1)); // <== create the copy: book5
+
+// book1.publishers[0] = {
+//   companyName: "Super Cool Company" // <== change the deep property of the book1
+// };
+// book1.author = "William Shakespeare"; // change the property of the book1
+// console.log(book1);
+// console.log(book5);
+
+//-----------------------------------------------------------------------
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
